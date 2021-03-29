cbec17e99d526911f9d43093dbdba9d9fb4fa928
sepehreslami <sepehr1381eslami@gmail.com>
2021-03-29 14:03:30 +0430

    init porject

---
+++ Assets/gc_network.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using gamingCloud;
+using gamingCloud.Network;
+
+public class gc_network : GCMultiPlayer 
+{
+
+    string status;
+    
+    void Start()
+    {
+        ConnectToMultiPlayerServer();
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 800, 20), status);
+    }
+
+    public override void ConnectedToServer()
+    {
+        status = "Connected to GC server!";
+        base.JoinToServer(new RoomSetting());
+    }
+
+    public override void OnJoined()
+    {
+        status = "Joined, roomId " + base.room.id;
+    }
+}
---
+++ Assets/test_move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_move : MonoBehaviour
+{
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.position = new Vector2(transform.position.x, transform.position.y + 2);
+        if (Input.GetKey(KeyCode.S))
+            transform.position = new Vector2(transform.position.x, transform.position.y - 2);
+        if (Input.GetKey(KeyCode.D))
+            transform.position = new Vector2(transform.position.x + 2, transform.position.y);
+        if (Input.GetKey(KeyCode.A))
+            transform.position = new Vector2(transform.position.x - 2, transform.position.y);
+    }
+}
